b6f0e1e5552db27caa5e7ff23a71b02a7866c9e4
Danieleseosa <danieleseosa946@gmail.com>
2025-08-20 05:21:47 -0700

    eight commit

--- app.js
+++ app.js
@@ -11,13 +11,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: ["http://localhost:5176"], // frontend dev URL
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
