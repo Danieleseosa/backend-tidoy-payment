6b5e68aa642703f1d9734a55d44ff60d89b2054a
Danieleseosa <danieleseosa946@gmail.com>
2025-09-30 09:22:55 -0700

    edit4

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -28,7 +28,7 @@ const initializePayment = async (req, res) => {
           email,
           amount, // already in kobo from frontend
           metadata: { bookingId },
-          callback_url: "http://localhost:5173/",
+          callback_url: "https://tidoy-web-frontend-final.vercel.app/",
         }),
       }
     );
